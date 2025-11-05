fcee88ff9713f124d12860f89bd9a18538d2c876
rkturer <rkt26@icloud.com>
2025-11-05 17:22:06 -0500

    Initial commit of hangman project

---
+++ gamestats.py
@@ -0,0 +1,27 @@
+#Rachel Turer --> Hangman Game using OOP
+class GameStats:
+    
+    def __init__(self):
+        """initalizes the health and remaining guesses for the user"""
+        self.health = 100
+        self.remaining_guesses = 5 
+        self.wrong_guesses = []
+        self.right_guesses = []
+    
+    def wrong_guess(self, guess):
+        """removes health and guesses for a wrong guess"""
+        self.health -= 20
+        self.remaining_guesses = self.health // 20  
+        self.wrong_guesses += guess
+    
+    def correct_guess(self, guess):
+        """gives back 5 percent health for correct guess"""
+        self.health += 5 
+        self.right_guesses += guess
+    
+    def __repr__(self):
+        return "You have guessed these letters correctly: " + str(self.right_guesses) + "\nYou have guessed these letters incorrectly: "  + str(self.wrong_guesses) + "\nYou have " + str(self.health) + "  percent health\nYou have " + str(self.remaining_guesses) + " guesses left"
+    
+    
+    
+    
---
+++ user.py
@@ -0,0 +1,98 @@
+from gamestats import *
+from guesser import *  
+import time 
+
+def user_interface():
+    hidden_phrase, phrase = phrase_collector()
+    hide_message()
+    display_menu()
+    stats = GameStats()
+    while True:
+        if stats.health <= 0:
+            print("Game Over\nThe secret phrase was: " + phrase)
+            x = end_game_sequence()
+        if "#" not in hidden_phrase:
+            print("CONGRATULATIONS\nYou Won")
+            x = end_game_sequence()
+            if x:
+                hidden_phrase, phrase = phrase_collector()
+                hide_message()
+                stats = GameStats()
+                display_menu()
+            else:
+                break
+                     
+        print("The hidden phrase: " + hidden_phrase)
+        print("\n")
+        x = input("Please select menu option: ")
+        
+        if x == "1":
+            guess = input("Please guess a letter: ").lower()
+            if guess in stats.wrong_guesses:
+                print("You have already guessed " + guess +"\n Please try again!")
+            else:
+                indexes = checker(phrase, guess)
+                if indexes == []:
+                    print(guess + " is not in the secret phrase")
+                    stats.wrong_guess(guess)
+                else:
+                    print(guess + " is in the secret phrase")
+                    stats.correct_guess(guess)
+                    hidden_phrase = replace(indexes, hidden_phrase, guess)
+        
+        elif x == "2":
+            guessed_phrase = input("Please guess the phrase: ").lower()
+            if guessed_phrase == phrase:
+                print("CONGRATULATIONS\nYou Won")
+                x = end_game_sequence()
+                if x:
+                    hidden_phrase, phrase = phrase_collector()
+                    hide_message()
+                    stats = GameStats()
+                    display_menu()
+                else:
+                    break
+            
+        elif x == "3":
+            print(stats.wrong_guesses)
+        
+        elif x== "4":
+            print(stats.right_guesses)
+        
+        elif x == "5":
+            print(stats.health)
+        
+        elif x == "6":
+            print("Thank you for playing")
+            break
+        
+        else:
+            print("Invalid Input. Please Try Again")
+        
+def end_game_sequence():
+    x = input("Do you want to play again (y/n) ").lower()
+    if x == "y":
+        return True
+    else:
+        return False
+
+def phrase_collector():
+    x = input("Please enter your secret phrase: ").lower()
+    hidden_phrase = formatter(x)
+    return hidden_phrase, x 
+
+def hide_message():
+    print("Hiding the secret message...")
+    time.sleep(3)
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    
+        
+def display_menu():
+    print("(1) Guess a letter ")
+    print("(2) Guess a the complete phrase ")
+    print("(3) Display incorrect guesses ")
+    print("(4) Display corrected guesses ")
+    print("(5) Display Health Stats ")
+    print("(6) Quit")
+    
+user_interface()
